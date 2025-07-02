929f49b78eeb0f31f4f33c42c4d7fdba942bb5d2
Kenean Tilahun <keni232127@gmail.com>
2025-07-02 21:17:26 +0300

    landingPage ingration

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar";
 import HeroSection from "./Components/HeroSection";
 import HowItWorks from "./Components/HowItWorks";
 import FeaturedBooks from "./Components/FeaturedBooks";
 import Footer from "./Components/Footer";
 
-const LandingPage = () => {
+const LandingPage: React.FC = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://bookshare-api.onrender.com/api/book/books/")
+      .then((res) => res.json())
+      .then((data) => setBooks(data))
+      .catch(() => setBooks([]));
+  }, []);
+
   return (
     <div className="bg-gray-50 text-gray-900">
       <Navbar />
       <HeroSection />
       <HowItWorks />
-      <FeaturedBooks />
+      <FeaturedBooks books={books} />
       <Footer />
     </div>
   );
