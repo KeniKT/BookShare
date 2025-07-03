b08abb97c2494f4789e47bd95d57b3dadefefb6c
Kenean Tilahun <keni232127@gmail.com>
2025-07-03 07:44:24 +0300

    eslint issue fixed

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // src/pages/dashboard/DashboardPage.tsx
 import React, { useState, useEffect } from 'react';
 import DashboardTabs from './Components/DashboardTabs';
